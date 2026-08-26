b2dc5871f0027e79a73214e2b3044c4a339f55a1
Cristiano <cbertolini@gmail.com>
2026-08-26 09:45:33 -0300

    test(cursos): fecha lacunas de cobertura em test_anexo.py
    Revisao apontou dois testes que passavam mesmo com a invariante quebrada:
    o teste de "link nao aceita arquivo" nunca setava arquivo (so testava url
    vazia) e o de "extensao mentirosa" usava um cabecalho nao reconhecido (so
    testava o ramo de conteudo desconhecido). Renomeia os dois para o que de
    fato provam e acrescenta os casos reais: link com url e arquivo juntos,
    anexo de arquivo com url, PNG de verdade batizado de .pdf, e dois
    caminhos felizes (arquivo e video bem formados) que faltavam por completo.
    
    Cada teste novo foi provado quebrando a guarda que ele cobre, rodando-o
    isolado e vendo falhar, antes de restaurar.

--- apps/cursos/tests/test_anexo.py
+++ apps/cursos/tests/test_anexo.py
@@ -20,9 +20,17 @@ def test_conteudo_desconhecido_nao_e_detectado():
     assert detecta_mime(b"nao sou arquivo conhecido") is None
 
 
-def test_extensao_mentirosa_e_recusada():
+def test_conteudo_nao_reconhecido_e_recusado():
     with pytest.raises(ValidationError):
         valida_upload("relatorio.pdf", tamanho=100, cabecalho=b"MZ\x90\x00 executavel")
+
+
+def test_extensao_mentirosa_e_recusada():
+    # Conteudo e um PNG de verdade (assinatura reconhecida), mas o nome mente
+    # dizendo que e um PDF: isto tem que cair no ramo de extensao incompativel,
+    # nao no de "conteudo desconhecido".
+    with pytest.raises(ValidationError):
+        valida_upload("x.pdf", tamanho=100, cabecalho=PNG)
 
 
 def test_pdf_acima_do_limite_e_recusado():
@@ -48,12 +56,28 @@ def entregavel_cards(dados_curso):
 
 
 @pytest.mark.django_db
-def test_anexo_de_link_nao_aceita_arquivo(entregavel_cards, aluno):
+def test_anexo_de_link_exige_url(entregavel_cards, aluno):
     anexo = Anexo(
         entregavel=entregavel_cards,
         tipo_midia=TipoMidia.LINK,
         titulo="Atividade no Scratch",
         url="",
+        enviado_por=aluno,
+    )
+    with pytest.raises(ValidationError):
+        anexo.full_clean()
+
+
+@pytest.mark.django_db
+def test_anexo_de_link_com_arquivo_e_recusado(entregavel_cards, aluno, arquivo_qualquer):
+    # Diferente do teste acima: aqui a url esta preenchida, entao so o ramo
+    # "link nao pode carregar arquivo" pode derrubar full_clean().
+    anexo = Anexo(
+        entregavel=entregavel_cards,
+        tipo_midia=TipoMidia.LINK,
+        titulo="Atividade no Scratch",
+        url="https://scratch.mit.edu/projects/123",
+        arquivo=arquivo_qualquer,
         enviado_por=aluno,
     )
     with pytest.raises(ValidationError):
@@ -73,6 +97,22 @@ def test_anexo_de_arquivo_exige_arquivo(entregavel_cards, aluno):
 
 
 @pytest.mark.django_db
+def test_anexo_de_arquivo_nao_aceita_url(entregavel_cards, aluno, arquivo_qualquer):
+    # Diferente do teste acima: aqui o arquivo esta preenchido, entao so o ramo
+    # "anexo de arquivo nao pode ter link" pode derrubar full_clean().
+    anexo = Anexo(
+        entregavel=entregavel_cards,
+        tipo_midia=TipoMidia.ARQUIVO,
+        titulo="Card 1",
+        arquivo=arquivo_qualquer,
+        url="https://exemplo.org/card.pdf",
+        enviado_por=aluno,
+    )
+    with pytest.raises(ValidationError):
+        anexo.full_clean()
+
+
+@pytest.mark.django_db
 def test_video_exige_duracao(entregavel_cards, aluno, arquivo_qualquer):
     anexo = Anexo(
         entregavel=entregavel_cards,
@@ -83,3 +123,28 @@ def test_video_exige_duracao(entregavel_cards, aluno, arquivo_qualquer):
     )
     with pytest.raises(ValidationError):
         anexo.full_clean()
+
+
+@pytest.mark.django_db
+def test_anexo_de_arquivo_bem_formado_passa_na_validacao(entregavel_cards, aluno, arquivo_qualquer):
+    anexo = Anexo(
+        entregavel=entregavel_cards,
+        tipo_midia=TipoMidia.ARQUIVO,
+        titulo="Card 1",
+        arquivo=arquivo_qualquer,
+        enviado_por=aluno,
+    )
+    anexo.full_clean()
+
+
+@pytest.mark.django_db
+def test_anexo_de_video_bem_formado_passa_na_validacao(entregavel_cards, aluno, arquivo_qualquer):
+    anexo = Anexo(
+        entregavel=entregavel_cards,
+        tipo_midia=TipoMidia.VIDEO,
+        titulo="Aula 1",
+        arquivo=arquivo_qualquer,
+        duracao_minutos=10,
+        enviado_por=aluno,
+    )
+    anexo.full_clean()
